c414ec674b287dec6370d8fc3cb00b0926d80f37
Lichtkind <lichtkind@cpan.org>
2025-06-08 17:01:25 +0200

    implemented lab

--- lib/Graphics/Toolkit/Color/Space/Instance/CIELAB.pm
+++ lib/Graphics/Toolkit/Color/Space/Instance/CIELAB.pm
@@ -18,36 +18,22 @@ my $eta = 0.008856 ;
 my $kappa = 903.3;
 
 sub from_xyz {
-    my ($x, $y, $z) = @{$_[0]};
-    my $fx = ($x > $eta) ? ($x ** (1/3)) : $kappa * $x;
-    my $fy = ($x > $eta) ? ($y ** (1/3)) : $kappa * $y;
-    my $fz = ($x > $eta) ? ($z ** (1/3)) : $kappa * $z;
-#   my $l = ($x > $eta) ? ((($x ** (1/3)) - 0.16) * 1.16) : $kappa * $x;
-
-    @xyz = map { $xyz[$_] / $D65[$_] } 0 .. 2;
-    @xyz = map { $_ > $eta ? ($_ ** (1/3)) : ((($kappa * $_) + .16) / 1.16) } @xyz;
-
-    return ($l, ($xyz[0] - $xyz[1] + 1) / 2, (($xyz[1] - $xyz[2] + 1) / 2)); # l a b
+    my ($xyz) = shift;
+    my @xyz = map {($_ > $eta) ? ($_ ** (1/3)) : ((($kappa * $_) + 16) / 116)} @$xyz;
+    my $l = (1.16 * $xyz[1]) - 0.16;
+    my $a = ($xyz[0] - $xyz[1] + 1) / 2;
+    my $b = ($xyz[1] - $xyz[2] + 1) / 2;
+    return ($l, $a, $b);
 }
 
 
 sub to_xyz {
-    my ($l, $a, $b) = @{$_[0]};
-    my $y = ($l + .16) / 1.16;
-    my $x = $y + (($a * 2)-1);
-    my $z = $y - (($b * 2)-1);
+    my ($lab) = shift;
+    my $fy = ($lab->[0] + 0.16) / 1.16;
+    my $fx = $fy - 1 + ($lab->[1] * 2);
+    my $fz = $fy + 1 - ($lab->[2] * 2);
 
-    $x = ($x**3 > $eta)            ? ($x ** 3) : ($kappa * (($x * 1.16) - .16));
-    $y = ($y**3 > ($eta * $kappa)) ? ($y ** 3) : ($kappa * $l);
-    $z = ($z**3 > $eta)            ? ($z ** 3) : ($kappa * (($z * 1.16) - .16));
-
-    $x *= $D65[0];
-    $z *= $D65[2];
-    my (@rgb) = mult_matrix([[ 3.2404542, -0.9692660,  0.0556434],
-                             [-1.5371385,  1.8760108, -0.2040259],
-                             [-0.4985314,  0.0415560,  1.0572252]], $x, $y, $z);
-
-    return ( map { remove_d65($_) } @rgb );
+    return ( map {my $f3 = $_** 3; ($f3 > $eta) ? $f3 : (( 116 * $_ - 16 ) / $kappa) } $fx, $fy, $fz );
 }
 
 $lab_def;

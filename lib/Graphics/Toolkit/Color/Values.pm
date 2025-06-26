2e87362bf76eefc55066d91e3f756c74f55d44f1
lichtkind <lichtkind@cpan.org>
2025-06-26 22:58:02 +0200

    implmented set and add

--- lib/Graphics/Toolkit/Color/Values.pm
+++ lib/Graphics/Toolkit/Color/Values.pm
@@ -1,111 +1,96 @@
 
-# value objects with cache of original values
+# stor of valus from a single color in RGB, original and name
 
 package Graphics::Toolkit::Color::Values;
 use v5.12;
 use warnings;
 use Graphics::Toolkit::Color::Name;
+use Graphics::Toolkit::Color::Space::Hub;
 
+my $RGB = Graphics::Toolkit::Color::Space::Hub::default_space();
 
-sub new { #  values => %space_name => tuple ,   ~origin_space, ~color_name
-    my ($pkg, $color_val) = @_;
-    my ($values, $space_name) = Graphics::Toolkit::Color::Space::Hub::deformat( $color_val );
-    return carp "could not recognize color value format" unless ref $values;
-    my $space = Graphics::Toolkit::Color::Space::Hub::get_space( $space_name );
-    my $std_space = Graphics::Toolkit::Color::Space::Hub::base_space();
-    my $self = {};
-    $self->{'origin'} = $space->name;
-    $values = $space->clamp( $values );
-    $values = $space->normalize( $values );
-    $self->{$space->name} = $values;
-    $self->{$std_space->name} = $space->convert($values, $std_space->name) if $space ne $std_space;
-    bless $self;
+sub new_from_any_input { #  values => %space_name => tuple ,   ~origin_space, ~color_name
+    my ($pkg, $color_def) = @_;
+    if (not ref $color_def){
+        my $rgb = Graphics::Toolkit::Color::Name::rgb_from_name( $color_def );
+        return bless { name => $color_def, rgb => $RGB->normalize( $rgb ), source => '', source_space => ''} if ref $rgb;
+    }
+    my ($values, $source_space, $source) = Graphics::Toolkit::Color::Space::Hub::convert_to_default_form( $color_def );
+    return "could not recognize color value format or color name: $color_def" unless ref $values;
+    my $name = Graphics::Toolkit::Color::Name::name_from_rgb( $RGB->denormalize( $values ) );
+    bless { name => $name, rgb => $values, source => $source, source_space => $source_space};
+}
+sub new_from_normal_tuple {
+    my ($pkg, $values) = @_;
+    #return 'need a normalized (0..1) RGB tuple' unless $RGB->range_check( $values, 'normal' );
+    $values = $RGB->clamp( $values, 'normal' );
+    my $name = Graphics::Toolkit::Color::Name::name_from_rgb( $RGB->denormalize( $values ) );
+    bless { name => $name, rgb => $values, source => '', source_space => ''};   
 }
 
-sub get_tuple { # get a value tuple in any color space, range and format
+########################################################################
+sub get_name { $_[0]->{'name'} }
+sub get_normal_tuple {
+    my ($self, $space_name) = @_;
+    Graphics::Toolkit::Color::Space::Hub::convert(
+        $self->{'rgb'}, $space_name, 1, $self->{'source_space'}, $self->{'source'},
+    );
+}
+sub get_formatted { # get a value tuple in any color space, range and format
     my ($self, $space_name, $format_name, $range_def) = @_;
-    Graphics::Toolkit::Color::Space::Hub::check_space_name( $space_name ) and return;
-    my $std_space_name = $Graphics::Toolkit::Color::Space::Hub::base_package;
-    $space_name //= $std_space_name;
-    my $space = Graphics::Toolkit::Color::Space::Hub::get_space( $space_name );
-    my $values = (exists $self->{$space->name})
-               ? $self->{$space->name}
-               : $space->deconvert( $self->{$std_space_name}, $std_space_name);
-    $values = $space->denormalize( $values, $range_def);
-    Graphics::Toolkit::Color::Space::Hub::format( $values, $space_name, $format_name);
+    $space_name //= Graphics::Toolkit::Color::Space::Hub::default_space_name();
+    my $color_space = Graphics::Toolkit::Color::Space::Hub::get_space( $space_name );
+    return "can not format values in unknown space name: $space_name" unless ref $color_space;
+    my $values = Graphics::Toolkit::Color::Space::Hub::convert (
+        $self->{'rgb'}, $space_name, defined $range_def, $self->{'source_space'}, $self->{'source'},
+    );
+    return $values unless ref $values;
+    $values = $color_space->denormalize( $values, $range_def );
+    $values = $color_space->format( $values, $format_name ) if defined $format_name;
+    return $values;
 }
-sub get_name { $_[0]->get( $_[0]->{'origin'}, 'string' ) }
-sub string   { $_[0]->get( $_[0]->{'origin'}, 'string' ) }
 
 ########################################################################
 
 sub set { # %val --> _
     my ($self, $val_hash) = @_;
-    my ($pos_hash, $space_name) = Graphics::Toolkit::Color::Space::Hub::partial_hash_deformat( $val_hash );
-    return carp 'key names: '.join(', ', keys %$val_hash). ' do not correlate to any supported color space' unless defined $space_name;
-    my @values = $self->get( $space_name );
+    my ($pos_hash, $space_name) = Graphics::Toolkit::Color::Space::Hub::deformat_partial_hash( $val_hash );
+    return 'key names: '.join(', ', keys %$val_hash). ' do not correlate to any supported color space' unless defined $space_name;
+    my $values = $self->get_formatted( $space_name );
     for my $pos (keys %$pos_hash){
-        $values[$pos] = $pos_hash->{ $pos };
+        $values->[$pos] = $pos_hash->{ $pos };
     }
-    __PACKAGE__->new([$space_name, @values]);
+    __PACKAGE__->new([$space_name, @$values]);
 }
 
 sub add { # %val --> _
     my ($self, $val_hash) = @_;
-    my ($pos_hash, $space_name) = Graphics::Toolkit::Color::Space::Hub::partial_hash_deformat( $val_hash );
-    return carp 'key names: '.join(', ', keys %$val_hash). ' do not correlate to any supported color space' unless defined $space_name;
-    my @values = $self->get( $space_name );
+    my ($pos_hash, $space_name) = Graphics::Toolkit::Color::Space::Hub::deformat_partial_hash( $val_hash );
+    return 'key names: '.join(', ', keys %$val_hash). ' do not correlate to any supported color space' unless defined $space_name;
+    my $values = $self->get_formatted( $space_name );
     for my $pos (keys %$pos_hash){
-        $values[$pos] += $pos_hash->{ $pos };
+        $values->[$pos] += $pos_hash->{ $pos };
     }
-    __PACKAGE__->new([$space_name, @values]);
+    __PACKAGE__->new([$space_name, @$values]);
 }
 
-sub blend { # _c1 _c2 -- +factor ~space --> _
-    my ($self, $c2, $factor, $space_name ) = @_;
+sub blend { # cv2 -- +percent, ~space --> _
+    my ($self, $c2, $percent, $space_name ) = @_;
     return carp "need value object as second argument" unless ref $c2 eq __PACKAGE__;
-    $factor //= 0.5;
+    $percent //= 50;
     $space_name //= 'RGB';
     Graphics::Toolkit::Color::Space::Hub::check_space_name( $space_name ) and return;
     my @values1 = $self->get( $space_name );
     my @values2 = $c2->get( $space_name );
-    my @rvalues = map { ((1-$factor) * $values1[$_]) + ($factor * $values2[$_]) } 0 .. $#values1;
+    my @rvalues = map { ((100 - $percent) * $values1[$_]) + ($percent * $values2[$_]) } 0 .. $#values1;
     __PACKAGE__->new([$space_name, @rvalues]);
 }
 
-########################################################################
-
 sub distance { # _c1 _c2 -- ~space ~select @range --> +
-    my ($self, $c2, $space_name, $select, $range) = @_;
-    return carp "need value object as second argument" unless ref $c2 eq __PACKAGE__;
-    $space_name //= 'RGB';
-    Graphics::Toolkit::Color::Space::Hub::check_space_name( $space_name ) and return;
-    my $space = Graphics::Toolkit::Color::Space::Hub::get_space( $space_name );
-    $select = $space->basis->shortcut_of_key($select) if $space->basis->is_key( $select );
-    my @values1 = $self->get( $space_name, 'list', 'normal' );
-    my @values2 = $c2->get( $space_name, 'list', 'normal' );
-    return unless defined $values1[0] and defined $values2[0];
-
-    my $delta = $space->delta( \@values1, \@values2 );
-    $delta = $space->denormalize_range( $delta, $range);
-    return unless ref $delta and @$delta == $space->dimensions;
-
-    # grep values for individual select / subspace distance
-    if (defined $select and $select){
-        my @components = split( '', $select );
-        my $pos = $space->basis->key_pos( $select );
-        @components = defined( $pos )
-                    ? ($pos)
-                    : (map  { $space->basis->shortcut_pos($_) }
-                       grep { defined $space->basis->shortcut_pos($_) } @components);
-        return - carp "called 'distance' for select $select that does not fit color space $space_name!" unless @components;
-        $delta = [ map { $delta->[$_] } @components ];
-    }
-
-    # Euclidean distance:
-    my $d = 0;
-    map {$d += ($_ * $_)} @$delta;
-    return sqrt $d;
+    my ($self, $cv2, $space_name, $select, $range) = @_;
+    return "need value object as second argument" unless ref $cv2 eq __PACKAGE__;
+    return "$space_name is not a color space name" if defined $space_name and not Graphics::Toolkit::Color::Space::is_space($space_name);
+    Graphics::Toolkit::Color::Space::Hub::distance( $self->{'rgb'}, $cv2->{'rgb'}, $space_name, $select, $range);
 }
 
 1;

adddd81afece9e300a60732791b751ceb0bcaa74
Lichtkind <lichtkind@cpan.org>
2025-08-26 23:56:36 +0200

    implement oklch

--- lib/Graphics/Toolkit/Color/Space/Instance/OKLCH.pm
+++ lib/Graphics/Toolkit/Color/Space/Instance/OKLCH.pm
@@ -6,7 +6,8 @@ use v5.12;
 use warnings;
 use Graphics::Toolkit::Color::Space qw/round_decimals/;
 
-my $TAU = 6.283185307;
+my $TAU     = 6.283185307;
+my $sqrt2_2 = sqrt(2) / 2;
 
 sub from_lab {
     my ($lab) = shift;
@@ -18,20 +19,20 @@ sub from_lab {
     my $c = sqrt( ($a**2) + ($b**2));
     my $h = atan2($b, $a);
     $h += $TAU if $h < 0;
-    return ([$lab->[0], $c / 539, $h / $TAU]);
+    return ([$lab->[0], $c / $sqrt2_2, $h / $TAU]);
 }
 sub to_lab {
     my ($lch) = shift;
-    my $a = $lch->[1] * cos($lch->[2] * $TAU) * 539;
-    my $b = $lch->[1] * sin($lch->[2] * $TAU) * 539;
-    return ([$lch->[0], ($a+500) / 1000, ($b+200) / 400 ]);
+    my $a = $lch->[1] * cos($lch->[2] * $TAU) * $sqrt2_2;
+    my $b = $lch->[1] * sin($lch->[2] * $TAU) * $sqrt2_2;
+    return ([$lch->[0], $a + .5, $b + .5 ]);
 }
 
 Graphics::Toolkit::Color::Space->new(
         name => 'OKLCH',
         axis => [qw/luminance chroma hue/],
         type => [qw/linear linear angular/],
-       range => [1, 539, 360],
-   precision => 3,
+       range => [[-.5, .5], [-.5, .5], 360],
+   precision => [5,5,2],
     convert => {OKLAB => [\&to_lab, \&from_lab]},
 );

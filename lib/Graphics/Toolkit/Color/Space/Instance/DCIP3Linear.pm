5a075628a9137741a0a33379085bb5648bdfd87c
Lichtkind <lichtkind@cpan.org>
2026-04-20 11:48:47 +0200

    remove bad UTF char from DCIPL space

--- lib/Graphics/Toolkit/Color/Space/Instance/DCIP3Linear.pm
+++ lib/Graphics/Toolkit/Color/Space/Instance/DCIP3Linear.pm
@@ -1,5 +1,5 @@
 
-# DCI-P3, 
+# DCI-P3
 
 package Graphics::Toolkit::Color::Space::Instance::DCIP3Linear;
 use v5.12;
@@ -26,13 +26,15 @@ Graphics::Toolkit::Color::Space->new(
      convert => {'Display-P3-Linear' => [\&to_dp3l, \&from_dp3l]},
 );
 
-DCI-P3 Linear → XYZ:
+__END__
+
+DCI-P3 Linear > XYZ:
 
 [ 0.4451698156   0.2771344092   0.1722826698 ]
 [ 0.2094916779   0.7215952542   0.0689130679 ]
 [ 0.0000000000   0.0470605601   0.9073553944 ]
 
-Inverse (XYZ → DCI-P3 Linear):
+Inverse (XYZ > DCI-P3 Linear):
 
 [  2.7253940305  -1.0180030062  -0.4401631952 ]
 [ -0.7951680258   1.6897320548   0.0226471906 ]

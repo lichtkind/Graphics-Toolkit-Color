cafed2cf282bbc45fa07e57c06d25c24a66cb1b0
Lichtkind <lichtkind@cpan.org>
2026-05-05 20:34:10 +0200

    add eeror handling class dummy

---
+++ lib/Graphics/Toolkit/Color/Error.pm
@@ -0,0 +1,13 @@
+
+# central hub of error handling
+
+package Graphics::Toolkit::Color::Error;
+
+use v5.12;
+use warnings;
+our @EXPORT = qw();
+our @EXPORT_OK = qw//;
+our %EXPORT_TAGS = (all => [@EXPORT_OK]);
+require Exporter;
+
+1;
